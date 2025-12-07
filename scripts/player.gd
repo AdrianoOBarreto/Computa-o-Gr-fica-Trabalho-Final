85e03cbd020a7e33965485ce3ba7689d5b854bf1
W-F-S <walkerfreitassantos@gmail.com>
2025-12-06 21:13:33 -0300

    adicionando pulo e gravidade, modificando terreno padrao, ainda precisamos adicionar modelos e o sistema de spaw

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,20 +5,34 @@ extends CharacterBody3D
 
 ## Speed of character movement
 const SPEED = 5.0
+const GRAVITY = 0.0
+const JUMP_VELOCITY = 5
 
 func _physics_process(_delta: float) -> void:
+	if not is_on_floor():
+		if Input.is_action_just_pressed("down"):
+			velocity.y -= GRAVITY * _delta
+		else:
+			velocity.y -= GRAVITY * _delta * 2
 
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	# We'll ignore up and down input, just using side to side
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+
+		
+	var input_dir := Input.get_vector("left", "right", "ui_up", "ui_down")
+
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.z, 0, SPEED)
+		
 
 	move_and_slide()
-
-	var collision = get_last_slide_collision()
-	if collision:
-		print("Collided with: ", collision.get_collider())
-		get_tree().quit()
+	
+	#    var collision = get_last_slide_collision()
+	#    if collision:
+	#	print("Collided with: ", collision.get_collider())
+	#	get_tree().quit()
--- scripts/terrain_controller.gd
+++ scripts/terrain_controller.gd
@@ -11,54 +11,64 @@ class_name TerrainController
 var TerrainBlocks: Array = []
 ## The set of terrian blocks which are currently rendered to viewport
 var terrain_belt: Array[MeshInstance3D] = []
-@export var terrain_velocity: float = 10.0
+@export var terrain_velocity: float = 2.0
 ## The number of blocks to keep rendered to the viewport
 @export var num_terrain_blocks = 4
 ## Path to directory holding the terrain block scenes
 @export_dir var terrian_blocks_path = "res://terrain_blocks"
 
+@onready var main_terrain: Resource  = preload("res://terrain_blocks/main_terrain.tscn")
+
+var current_terrain_instance: Node
 
 func _ready() -> void:
-	_load_terrain_scenes(terrian_blocks_path)
-	_init_blocks(num_terrain_blocks)
+	spaw_terrain()
 
+func spaw_terrain() -> void:
+	var terrain_instance: Node = main_terrain.instantiate()
+	add_child(terrain_instance)
+	terrain_instance.position = Vector3(1, 1, -20)
+	current_terrain_instance  = terrain_instance
 
 func _physics_process(delta: float) -> void:
 	_progress_terrain(delta)
 
 
-func _init_blocks(number_of_blocks: int) -> void:
-	for block_index in number_of_blocks:
-		var block = TerrainBlocks.pick_random().instantiate()
-		if block_index == 0:
-			block.position.z = block.mesh.size.y/2
-		else:
-			_append_to_far_edge(terrain_belt[block_index-1], block)
-		add_child(block)
-		terrain_belt.append(block)
+#func _init_blocks(number_of_blocks: int) -> void:
+#	for block_index in number_of_blocks:
+#		var block = TerrainBlocks.pick_random().instantiate()
+#		if block_index == 0:
+#			block.position.z = block.mesh.size.y/2
+#		else:
+#			_append_to_far_edge(terrain_belt[block_index-1], block)
+#		add_child(block)
+#		terrain_belt.append(block)
 
 
 func _progress_terrain(delta: float) -> void:
-	for block in terrain_belt:
-		block.position.z += terrain_velocity * delta
+	current_terrain_instance.position.z += terrain_velocity * delta
+	print(current_terrain_instance.position.z)
+	if(current_terrain_instance.position.z > 15):
+		current_terrain_instance.position = Vector3(1, 1, 20)
 
-	if terrain_belt[0].position.z >= terrain_belt[0].mesh.size.y/2:
-		var last_terrain = terrain_belt[-1]
-		var first_terrain = terrain_belt.pop_front()
-
-		var block = TerrainBlocks.pick_random().instantiate()
-		_append_to_far_edge(last_terrain, block)
-		add_child(block)
-		terrain_belt.append(block)
-		first_terrain.queue_free()
-
-
-func _append_to_far_edge(target_block: MeshInstance3D, appending_block: MeshInstance3D) -> void:
-	appending_block.position.z = target_block.position.z - target_block.mesh.size.y/2 - appending_block.mesh.size.y/2
+#	if terrain_belt[0].position.z >= terrain_belt[0].mesh.size.y/2:
+#		var last_terrain = terrain_belt[-1]
+#		var first_terrain = terrain_belt.pop_front()
+#
+#		var block = TerrainBlocks.pick_random().instantiate()
+#		_append_to_far_edge(last_terrain, block)
+#		add_child(block)
+#		terrain_belt.append(block)
+#		first_terrain.queue_free()
 
 
-func _load_terrain_scenes(target_path: String) -> void:
-	var dir = DirAccess.open(target_path)
-	for scene_path in dir.get_files():
-		print("Loading terrian block scene: " + target_path + "/" + scene_path)
-		TerrainBlocks.append(load(target_path + "/" + scene_path))
+#func _append_to_far_edge(target_block: MeshInstance3D, appending_block: MeshInstance3D) -> void:
+#	appending_block.position.z = target_block.position.z - target_block.mesh.size.y/2 - appending_block.mesh.size.y/2
+
+
+#func _load_terrain_scenes(target_path: String) -> void:
+	#var dir = DirAccess.open(target_path)
+	#for scene_path in dir.get_files():
+	#	print("Loading terrian block scene: " + target_path + "/" + scene_path)
+	#	TerrainBlocks.append(load(target_path + "/" + scene_path))
+	
